26f84c2a8708b8e9afb1b26193b91402ff2fda87
Jet Balagtas <jetbalagtas@gmail.com>
2018-11-07 13:25:56 -0500

    initial commit

---
+++ public/src/js/feed.js
@@ -0,0 +1,16 @@
+/* eslint-disable require-jsdoc */
+var shareImageButton = document.querySelector('#share-image-button');
+var createPostArea = document.querySelector('#create-post');
+var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+
+function openCreatePostModal() {
+  createPostArea.style.display = 'block';
+}
+
+function closeCreatePostModal() {
+  createPostArea.style.display = 'none';
+}
+
+shareImageButton.addEventListener('click', openCreatePostModal);
+
+closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
