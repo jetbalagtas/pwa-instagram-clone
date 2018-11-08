189817067122d1c35dc3ce4f37ba74d262317df1
Jet Balagtas <jetbalagtas@gmail.com>
2018-11-08 12:41:01 -0500

    add a fetch event listener

--- public/sw.js
+++ public/sw.js
@@ -6,3 +6,8 @@ self.addEventListener('activate', function(event) {
   console.log('[Service Worker] Activating Service Worker...', event);
   return self.clients.claim();
 });
+
+self.addEventListener('fetch', function(event) {
+  console.log('[Service Worker] Fetching something...', event);
+  event.respondWith(fetch(event.request));
+});
