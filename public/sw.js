b788175e46c93a8d85554be7d47602bb7b725529
Jet Balagtas <jetbalagtas@gmail.com>
2018-11-08 11:58:42 -0500

    install and activate service worker

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker...', event);
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker...', event);
+  return self.clients.claim();
+});
