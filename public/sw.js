b5373eae782b13ad009afeae01655bce3347ee9d
Jet Balagtas <jetbalagtas@gmail.com>
2018-11-15 20:35:10 -0500

    trim/limit cache size

--- public/sw.js
+++ public/sw.js
@@ -17,6 +17,17 @@ const STATIC_FILES = [
   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
 ];
 
+const trimCache = (cacheName, maxItems) => {
+  caches.open(cacheName)
+  .then(cache => cache.keys()
+  .then(keys => {
+    if (keys.length > maxItems) {
+      cache.delete(keys[0])
+      .then(trimCache(cacheName, maxItems));
+    }
+  }));
+}
+
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker...', event);
   event.waitUntil(
@@ -59,6 +70,7 @@ self.addEventListener('fetch', function(event) {
       caches.open(CACHE_DYNAMIC_NAME)
       .then(cache => fetch(event.request)
       .then(res => {
+        trimCache(CACHE_DYNAMIC_NAME, 3);
         cache.put(event.request, res.clone());
         return res;
       }))
@@ -75,6 +87,7 @@ self.addEventListener('fetch', function(event) {
           return fetch(event.request)
           .then(res => caches.open(CACHE_DYNAMIC_NAME)
           .then(cache => {
+            trimCache(CACHE_DYNAMIC_NAME, 3);
             cache.put(event.request.url, res.clone())
             return res;
           }))
