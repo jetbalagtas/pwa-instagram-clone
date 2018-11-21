34a06a69e2896005aeafcfbadb7211dc650d4458
Jet Balagtas <jetbalagtas@gmail.com>
2018-11-21 13:13:59 -0500

    notifs: add (same) tag to stack notifs, and renotify for new notifs

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -33,7 +33,9 @@ const displayConfirmationNotification = () => {
       dir: 'ltr',
       lang: 'en-US', // BCP 47
       vibrate: [100, 50, 200],
-      badge: '/src/images/icons/app-icon-96x96.png'
+      badge: '/src/images/icons/app-icon-96x96.png',
+      tag: 'confirm-notification',
+      renotify: true
     };
     navigator.serviceWorker.ready
     .then(swreg => swreg.showNotification('PWAGram: Success!', options))
