02193a3072a3c75ff7cdb19069f22294c510e6af
Jet Balagtas <jetbalagtas@gmail.com>
2018-11-21 13:21:10 -0500

    notifs: add actions

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -35,7 +35,11 @@ const displayConfirmationNotification = () => {
       vibrate: [100, 50, 200],
       badge: '/src/images/icons/app-icon-96x96.png',
       tag: 'confirm-notification',
-      renotify: true
+      renotify: true,
+      actions: [
+        { action: 'confirm', title: 'Okay', icon: '/src/images/icons/app-icon-96x96.png' },
+        { action: 'cancel', title: 'Cancel', icon: '/src/images/icons/app-icon-96x96.png' }
+      ]
     };
     navigator.serviceWorker.ready
     .then(swreg => swreg.showNotification('PWAGram: Success!', options))
