916d5a4ad6522a6418ebbd620aa0c35b02eb6053
Jet Balagtas <jetbalagtas@gmail.com>
2018-11-25 18:02:18 -0500

    notifs: listen to push messages

--- public/sw.js
+++ public/sw.js
@@ -211,3 +211,22 @@ self.addEventListener('notificationclick', function(event) {
 self.addEventListener('notificationclose', function(event) {
   console.log('Notification was closed', event);
 });
+
+self.addEventListener('push', function(event) {
+  console.log('Push Notification received', event);
+  let data = { title: 'New!', content: 'Something new happened!' };
+
+  if (event.data) {
+    data = JSON.parse(event.data.text());
+  }
+
+  const options = {
+    body: data.content,
+    icon: '/src/images/icons/app-icon-96x96.png',
+    badge: '/src/images/icons/app-icon-96x96.png',
+  }
+
+  event.waitUntil(
+    self.registration.showNotification(data.title, options)
+  );
+});
