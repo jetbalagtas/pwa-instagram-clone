0408eaaae810ca9d535d0e51c21a35334658ec54
Jet Balagtas <jetbalagtas@gmail.com>
2018-11-30 16:09:17 -0500

    workbox: add cache expiration option to google-fonts

--- public/sw-base.js
+++ public/sw-base.js
@@ -8,7 +8,13 @@ workbox.routing.registerRoute(/.*(?:firebasestorage\.googleapis)\.com.*$/, workb
 }));
 
 workbox.routing.registerRoute(/.*(?:googleapis|gstatic)\.com.*$/, workbox.strategies.staleWhileRevalidate({
-  cacheName: 'google-fonts'
+  cacheName: 'google-fonts',
+  plugins: [
+    new workbox.expiration.Plugin({
+      maxEntries: 3,
+      maxAgeSeconds: 60 * 60 * 24 * 30
+    })
+  ]
 }));
 
 workbox.routing.registerRoute('https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css', workbox.strategies.staleWhileRevalidate({
