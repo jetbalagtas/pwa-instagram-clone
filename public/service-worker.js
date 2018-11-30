d537023b93b93cfc3831aaebd4e9234758d40c1c
Jet Balagtas <jetbalagtas@gmail.com>
2018-11-30 15:15:11 -0500

    workbox: expand dynamic caching, separate image cache from font cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,18 @@
 /* eslint-disable quotes */
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 
 workbox.precaching.suppressWarnings();
 
+workbox.routing.registerRoute(/.*(?:firebasestorage\.googleapis)\.com.*$/, workbox.strategies.staleWhileRevalidate({
+  cacheName: 'post-images'
+}));
+
 workbox.routing.registerRoute(/.*(?:googleapis|gstatic)\.com.*$/, workbox.strategies.staleWhileRevalidate({
   cacheName: 'google-fonts'
+}));
+
+workbox.routing.registerRoute('https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css', workbox.strategies.staleWhileRevalidate({
+  cacheName: 'material-css'
 }));
 
 workbox.precaching.precacheAndRoute([
@@ -70,7 +78,7 @@ workbox.precaching.precacheAndRoute([
   },
   {
     "url": "sw-base.js",
-    "revision": "d140cf8b94a066c4fd701a129538f647"
+    "revision": "a59890e32d1821b2a02960854b87b402"
   },
   {
     "url": "sw.js",
--- public/sw-base.js
+++ public/sw-base.js
@@ -1,10 +1,18 @@
 /* eslint-disable quotes */
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 
 workbox.precaching.suppressWarnings();
 
+workbox.routing.registerRoute(/.*(?:firebasestorage\.googleapis)\.com.*$/, workbox.strategies.staleWhileRevalidate({
+  cacheName: 'post-images'
+}));
+
 workbox.routing.registerRoute(/.*(?:googleapis|gstatic)\.com.*$/, workbox.strategies.staleWhileRevalidate({
   cacheName: 'google-fonts'
+}));
+
+workbox.routing.registerRoute('https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css', workbox.strategies.staleWhileRevalidate({
+  cacheName: 'material-css'
 }));
 
 workbox.precaching.precacheAndRoute([]);
