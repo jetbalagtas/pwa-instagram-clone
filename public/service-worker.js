ca9665256dab2620fe5e15273d835fc077803710
Jet Balagtas <jetbalagtas@gmail.com>
2018-12-03 15:50:59 -0500

    cleanup and globignore 404 page

--- public/service-worker.js
+++ public/service-worker.js
@@ -56,10 +56,6 @@ workbox.routing.registerRoute(routeData => routeData.event.request.headers
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "404.html",
-    "revision": "0a27a4163254fc8fce870c8cc3a3f94f"
-  },
-  {
     "url": "favicon.ico",
     "revision": "2cab47d9e04d664d93c8d91aec59e812"
   },
@@ -165,56 +161,56 @@ self.addEventListener('sync', function(event) {
       })  
     );
   }
- });
- 
- self.addEventListener('notificationclick', function(event) {
-   const notification = event.notification;
-   const action = event.action;
- 
-   console.log(notification);
-   
-   if (action === 'confirm') {
-     console.log('Confirm was chosen.');
-   } else {
-     console.log(action);
-     event.waitUntil(
-       clients.matchAll()
-       .then(clis => {
-         const client = clis.find(c => c.visibilityState === 'visible');
-         if (client !== undefined) {
-           client.navigate(notification.data.url);
-           client.focus();
-         } else {
-           clients.openWindow(notification.data.url);
-         }
-         notification.close();
-       })
-     );
-   }
- });
- 
- self.addEventListener('notificationclose', function(event) {
-   console.log('Notification was closed', event);
- });
- 
- self.addEventListener('push', function(event) {
-   console.log('Push Notification received', event);
-   let data = { title: 'New!', content: 'Something new happened!', openUrl: '/' };
- 
-   if (event.data) {
-     data = JSON.parse(event.data.text());
-   }
- 
-   const options = {
-     body: data.content,
-     icon: '/src/images/icons/app-icon-96x96.png',
-     badge: '/src/images/icons/app-icon-96x96.png',
-     data: {
-       url: data.openUrl
-     }
-   }
- 
-   event.waitUntil(
-     self.registration.showNotification(data.title, options)
-   );
- });
+});
+
+self.addEventListener('notificationclick', function(event) {
+  const notification = event.notification;
+  const action = event.action;
+
+  console.log(notification);
+  
+  if (action === 'confirm') {
+    console.log('Confirm was chosen.');
+  } else {
+    console.log(action);
+    event.waitUntil(
+      clients.matchAll()
+      .then(clis => {
+        const client = clis.find(c => c.visibilityState === 'visible');
+        if (client !== undefined) {
+          client.navigate(notification.data.url);
+          client.focus();
+        } else {
+          clients.openWindow(notification.data.url);
+        }
+        notification.close();
+      })
+    );
+  }
+});
+
+self.addEventListener('notificationclose', function(event) {
+  console.log('Notification was closed', event);
+});
+
+self.addEventListener('push', function(event) {
+  console.log('Push Notification received', event);
+  let data = { title: 'New!', content: 'Something new happened!', openUrl: '/' };
+
+  if (event.data) {
+    data = JSON.parse(event.data.text());
+  }
+
+  const options = {
+    body: data.content,
+    icon: '/src/images/icons/app-icon-96x96.png',
+    badge: '/src/images/icons/app-icon-96x96.png',
+    data: {
+      url: data.openUrl
+    }
+  }
+
+  event.waitUntil(
+    self.registration.showNotification(data.title, options)
+  );
+});
