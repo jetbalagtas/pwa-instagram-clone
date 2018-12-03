f2b124e617ee5fe9675e5305fb6540709b65a539
Jet Balagtas <jetbalagtas@gmail.com>
2018-12-03 12:51:34 -0500

    workbox: handle background sync and push notif

--- public/sw-base.js
+++ public/sw-base.js
@@ -55,3 +55,89 @@ workbox.routing.registerRoute(routeData => routeData.event.request.headers
   }));
 
 workbox.precaching.precacheAndRoute([]);
+
+self.addEventListener('sync', function(event) {
+  console.log('[Service Worker] Background syncing...', event);
+  if (event.tag === 'sync-new-posts') {
+    console.log('[Service Worker] Syncing new Post...');
+    event.waitUntil(
+      readAllData('sync-posts')
+      .then(data => {
+        for (const dt of data) {
+          const postData = new FormData();
+          postData.append('id', dt.id);
+          postData.append('title', dt.title);
+          postData.append('location', dt.location);
+          postData.append('rawLocationLat', dt.rawLocation.lat);
+          postData.append('rawLocationLng', dt.rawLocation.lng);
+          postData.append('file', dt.picture, dt.id + '.png');
+          
+          fetch('https://us-central1-pwa-instagram-clone.cloudfunctions.net/storePostData', {
+            method: 'POST',
+            body: postData
+          })
+          .then(res => {
+            console.log('Sent data', res);
+            if (res.ok) {
+              res.json()
+              .then(resData => deleteItemFromData('sync-posts', resData.id));
+            }
+         })
+         .catch(err => console.log('Error while sending data.', err));
+        }
+      })  
+    );
+  }
+ });
+ 
+ self.addEventListener('notificationclick', function(event) {
+   const notification = event.notification;
+   const action = event.action;
+ 
+   console.log(notification);
+   
+   if (action === 'confirm') {
+     console.log('Confirm was chosen.');
+   } else {
+     console.log(action);
+     event.waitUntil(
+       clients.matchAll()
+       .then(clis => {
+         const client = clis.find(c => c.visibilityState === 'visible');
+         if (client !== undefined) {
+           client.navigate(notification.data.url);
+           client.focus();
+         } else {
+           clients.openWindow(notification.data.url);
+         }
+         notification.close();
+       })
+     );
+   }
+ });
+ 
+ self.addEventListener('notificationclose', function(event) {
+   console.log('Notification was closed', event);
+ });
+ 
+ self.addEventListener('push', function(event) {
+   console.log('Push Notification received', event);
+   let data = { title: 'New!', content: 'Something new happened!', openUrl: '/' };
+ 
+   if (event.data) {
+     data = JSON.parse(event.data.text());
+   }
+ 
+   const options = {
+     body: data.content,
+     icon: '/src/images/icons/app-icon-96x96.png',
+     badge: '/src/images/icons/app-icon-96x96.png',
+     data: {
+       url: data.openUrl
+     }
+   }
+ 
+   event.waitUntil(
+     self.registration.showNotification(data.title, options)
+   );
+ });
